440b752de1100c458a2ab19df01a71264119e42f
Lucas <65600775+Lukyyy9@users.noreply.github.com>
2023-12-12 10:11:09 +0100

    init3

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// Création d'un moteur Matter.js
-const Engine = Matter.Engine;
-const Render = Matter.Render;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+// Importation des modules Matter.js
+const { Engine, Render, World, Bodies, Constraint } = Matter;
 
+// Création du moteur
 const engine = Engine.create();
 
-// Création d'un rendu
+// Création du rendu
 const render = Render.create({
     element: document.body,
     engine: engine,
@@ -36,8 +33,37 @@ for (let i = 0; i < 10; i++) {
 // Ajout des boules au monde
 World.add(engine.world, balls);
 
+// Création d'une croix composée de plusieurs corps
+const crossBodies = [];
+
+// Création des bras de la croix
+const armOptions = {
+    render: {
+        fillStyle: 'green' // Choisissez la couleur souhaitée
+    }
+};
+
+const arm1 = Bodies.rectangle(400, 400, 20, 80, armOptions);
+const arm2 = Bodies.rectangle(400, 400, 80, 20, armOptions);
+
+// Ajout des bras à la croix
+crossBodies.push(arm1, arm2);
+
+// Ajout des bras au monde
+World.add(engine.world, crossBodies);
+
+// Création de la contrainte pour permettre la rotation de la croix
+const constraintOptions = {
+    pointA: { x: 400, y: 400 },
+    bodyB: arm2,
+    length: 0
+};
+
+const constraint = Constraint.create(constraintOptions);
+World.add(engine.world, constraint);
+
 // Démarrage du moteur
-Engine.run(engine);
+Matter.Runner.run(engine)
 
 // Démarrage du rendu
 Render.run(render);
